cd7defa1d30c2f01bd0df95cba127faf5854bd43
notebook_CBD <orientpine@gmail.com>
2022-06-14 21:13:42 -0400

    캘린더앱 기본 구조는 완성함

--- calenderscheduler/lib/component/schedule_bottom_sheet.dart
+++ calenderscheduler/lib/component/schedule_bottom_sheet.dart
@@ -10,37 +10,42 @@ class ScheduleBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
-    return Container(
-      color: lightColorScheme.background,
-      height: (MediaQuery.of(context).size.height) / 2 + bottomInset,
-      child: Padding(
-        padding: EdgeInsets.only(
-          bottom: bottomInset,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 8,
-            right: 8,
-            top: 16,
-            bottom: 0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _Time(),
-              SizedBox(
-                height: 10,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: SafeArea(
+        child: Container(
+          color: lightColorScheme.background,
+          height: (MediaQuery.of(context).size.height) / 2 + bottomInset,
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: bottomInset,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                right: 8,
+                top: 16,
+                bottom: 0,
               ),
-              _Contents(),
-              SizedBox(
-                height: 10,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  _Time(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  _Contents(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  _ColorPicker(),
+                  _SaveButton(),
+                ],
               ),
-              _ColorPicker(),
-              SizedBox(
-                height: 10,
-              ),
-              _SaveButton(),
-            ],
+            ),
           ),
         ),
       ),
@@ -57,11 +62,21 @@ class _Time extends StatelessWidget {
       // ignore: prefer_const_literals_to_create_immutables
       children: [
         // text field는 row일 때 항상 size를 정해줘야한다!s
-        Expanded(child: CustomTextField(title: '시작 시간')),
+        Expanded(
+          child: CustomTextField(
+            title: '시작 시간',
+            isTime: true,
+          ),
+        ),
         SizedBox(
           width: 20,
         ),
-        Expanded(child: CustomTextField(title: '종료 시간')),
+        Expanded(
+          child: CustomTextField(
+            title: '종료 시간',
+            isTime: true,
+          ),
+        ),
       ],
     );
   }
@@ -72,7 +87,12 @@ class _Contents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomTextField(title: '내용');
+    return Expanded(
+      child: CustomTextField(
+        title: '내용',
+        isTime: false,
+      ),
+    );
   }
 }
 
@@ -92,6 +112,7 @@ class _ColorPicker extends StatelessWidget {
           renderColor(Colors.yellow),
           renderColor(Colors.green),
           renderColor(Colors.blue),
+          renderColor(Colors.indigo),
           renderColor(Colors.purple),
         ],
       ),
